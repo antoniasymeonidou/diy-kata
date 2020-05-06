5a22a4b04a70a040cc46e7ca71b4feba8e314e00
Antonia Symeonidou <antonia.symeonidou@outlook.com>
2020-05-06 16:30:27 +0100

    finished humandogcatyears

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+    const humanYears = 10;
+    
+    test("returns an array of three numbers when humanCatDogYears is passed a number.", () => {
+      expect(humanCatDogYears(humanYears)).toEqual([humanYears, 56, 68]);
+    });
+  });
